bd85ac3d7ed6f48c7225dc7c98386956ac9fcbfd
scaruuu <scaru2105@gmail.com>
2024-07-01 18:28:29 +0530

    forgot .gitignore hahahah

---
+++ target/debug/deps/webapp_rs.d
@@ -0,0 +1,5 @@
+D:\rust\webapp-rs\target\debug\deps\webapp_rs.exe: src/main.rs
+
+D:\rust\webapp-rs\target\debug\deps\webapp_rs.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/webapp-rs.d
@@ -0,0 +1,1 @@
+D:\rust\webapp-rs\target\debug\webapp-rs.exe: D:\rust\webapp-rs\src\main.rs
