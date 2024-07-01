5ff4880d80295541ac09866f3b8308da4ffe787c
scaruuu <scaru2105@gmail.com>
2024-07-01 18:15:45 +0530

    initial commit

---
+++ target/debug/deps/webapp_rs-17d422d9053740b9.d
@@ -0,0 +1,5 @@
+D:\rust\webapp-rs\target\debug\deps\libwebapp_rs-17d422d9053740b9.rmeta: src/main.rs
+
+D:\rust\webapp-rs\target\debug\deps\webapp_rs-17d422d9053740b9.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/webapp_rs-b87a5669c71d7463.d
@@ -0,0 +1,5 @@
+D:\rust\webapp-rs\target\debug\deps\libwebapp_rs-b87a5669c71d7463.rmeta: src/main.rs
+
+D:\rust\webapp-rs\target\debug\deps\webapp_rs-b87a5669c71d7463.d: src/main.rs
+
+src/main.rs:
